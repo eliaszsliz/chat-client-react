0df3b75e9a600472168139cc4f713d2854bcf799
hauczak <hauczak@hotmail.com>
2019-01-08 13:25:31 +0100

    add readable date to meesages

--- src/components/ChatWindowMessage.js
+++ src/components/ChatWindowMessage.js
@@ -3,9 +3,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function ChatWindowMessage(props) {
+  const { date } = props;
+  const hh = `0${date.getHours()}`.slice(-2);
+  const mm = `0${date.getMinutes()}`.slice(-2);
+  const readableDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} - ${hh}:${mm}`;
+
   return (<MessageWrapper>
     <Message origin={props.origin}>
-      <b>{props.author}</b> {props.body}
+      <b>{props.author} - {readableDate}</b> {props.body}
     </Message>
   </MessageWrapper>)
 }
